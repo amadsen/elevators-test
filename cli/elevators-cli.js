14bae3767de6c7bec32d707c0cc8827633a6c84c
Aaron Madsen <amadsen@codifiant.com>
2016-10-08 18:44:36 -0600

    1.5 hrs

--- cli/elevators-cli.js
+++ cli/elevators-cli.js
@@ -32,4 +32,22 @@ var defaultConfig = {
 var elevatorSys = require('../'),
   cfg = extend({}, defaultConfig, yargs.argv);
 
+// Call - request elevator to floor, to go up or down
+
+// elevator: Open - open elevator (for either loading or unloading) - elevator
+// does this on its own at the proper floor, no command needed. However, this
+// could be used to simulate an "open door" button on the inside of the elevator.
+
+// elevator: GoTo - request elevator take passenger to floor. Marks the elevator
+// as occupied until the floor is reached. This can only be done if you have an
+// elevator (glossing over the process of loading the elevator. The system does
+// not care whether anyone got on or off the elevator. It only cares that one of
+// the elevator's internal buttons got pressed)
+
 elevatorSys.init(cfg);
+repl.start({
+  ignoreUndefined: true,
+  eval: function(cmd, context, filename, cb){
+
+  }
+});
